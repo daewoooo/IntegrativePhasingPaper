82da2b57bca167f45cdb6d10bafb0fee2e04e1da
daewoooo <daewoooo@gmail.com>
2017-05-24 11:13:54 +0200

    updating pipeline file

--- StrandPhaseR_pipeline.R
+++ StrandPhaseR_pipeline.R
@@ -1,4 +1,8 @@
+#!/usr/bin/Rscript
 
 require('StrandPhaseR')
 require('BSgenome.Hsapiens.UCSC.hg19')
-strandPhaseR(inputfolder=<StrandSeq_bams> , outputfolder=<user_defined> , numCPU = 1, WCregions = <WCregions_list> , positions='Platinum_NA12878_SNP_allChroms', pairedEndReads=TRUE, chromosomes=c(1:22), min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele='NA12878_merged.bam', splitPhasedReads=TRUE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
+
+args=commandArgs(TRUE)
+
+strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], numCPU = 1, pairedEndReads=TRUE, chromosomes=c(1:22), min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=NULL, splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
