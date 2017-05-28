b4b316db8eaef6822574b9563517342cb797e0f6
daewoooo <daewoooo@gmail.com>
2017-05-28 11:12:45 +0200

    fixed bug in Rscript

--- StrandS_suppData/StrandPhaseR_pipeline.R
+++ StrandS_suppData/StrandPhaseR_pipeline.R
@@ -1,11 +1,11 @@
 #!/usr/bin/Rscript
 
+args=commandArgs(TRUE)
+
 #add user defined path to load needed libraries
-.libPaths( c( .libPaths(), "/local/data/strand-seq/pipeline/revision") )
+.libPaths( c( .libPaths(), args[6]) )
 
 suppressPackageStartupMessages(library(StrandPhaseR))
 suppressPackageStartupMessages(library(BSgenome.Hsapiens.UCSC.hg19))
-
-args=commandArgs(TRUE)
 
 strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], chromosomes=args[5], numCPU = 1, pairedEndReads=TRUE, min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=NULL, splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
