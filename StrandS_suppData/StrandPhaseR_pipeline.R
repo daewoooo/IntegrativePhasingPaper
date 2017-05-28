9a0d2930f780780ab58ba9271f2342a7c3494410
daewoooo <daewoooo@gmail.com>
2017-05-28 09:46:16 +0200

    updated chromosome argument

--- StrandS_suppData/StrandPhaseR_pipeline.R
+++ StrandS_suppData/StrandPhaseR_pipeline.R
@@ -5,4 +5,4 @@ require('BSgenome.Hsapiens.UCSC.hg19')
 
 args=commandArgs(TRUE)
 
-strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], numCPU = 1, pairedEndReads=TRUE, chromosomes=c(1:22), min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=NULL, splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
+strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], chromosomes=args[5], numCPU = 1, pairedEndReads=TRUE, min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=NULL, splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
