b6b44ffbf7d22a5042ebac67c6fc5a4c84f68111
daewoooo <daewoooo@gmail.com>
2017-05-30 16:19:27 +0200

    added new argument

--- StrandS_suppData/StrandPhaseR_pipeline.R
+++ StrandS_suppData/StrandPhaseR_pipeline.R
@@ -8,4 +8,4 @@ args=commandArgs(TRUE)
 suppressPackageStartupMessages(library(StrandPhaseR))
 suppressPackageStartupMessages(library(BSgenome.Hsapiens.UCSC.hg19))
 
-strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], chromosomes=args[5], numCPU = 1, pairedEndReads=TRUE, min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=NULL, splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
+strandPhaseR(inputfolder=args[1], outputfolder=args[2], WCregions = args[3] , positions=args[4], chromosomes=args[5], numCPU = 1, pairedEndReads=TRUE, min.mapq=10, min.baseq=20, num.iterations=2, translateBases=TRUE, fillMissAllele=args[7], splitPhasedReads=FALSE, exportVCF='NA12878',callBreaks=FALSE, bsGenome=BSgenome.Hsapiens.UCSC.hg19)
